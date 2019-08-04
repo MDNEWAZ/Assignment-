a727017129ce4aaab11cc80568010cd491c85fe3
Shah Newaz <shahnewaz385@gmail.com>
2019-08-04 23:31:11 +0600

    Preoblem-196

---
+++ BreakSimulator/BreakSimulator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BreakSimulator
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            Double v, t, n;
+
+            for (int i = 0; i < T; i++)
+            {
+                string first = Console.ReadLine();
+
+                string[] array = first.Split();
+
+                v = Double.Parse(array[0]);
+
+                t = Double.Parse(array[1]);
+
+                n = -(v / t);
+
+                Console.WriteLine($"{n:F2}");
+
+            }
+        }
+    }
+}
